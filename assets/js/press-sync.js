70cfd3e35b787d8af6f98de753473736659bfce8
Derek Chafin <infomaniac50@gmail.com>
2022-05-03 16:58:02 -0500

    Code cleanup

--- assets/js/press-sync.js
+++ assets/js/press-sync.js
@@ -1,145 +1,144 @@
 window.PressSync = ( function( window, document, $ ) {
 
-	var app = {};
+    const app = {};
 
-	app.PAGE_SIZE = 1;
+    app.PAGE_SIZE = 1;
 
-	app.init = function() {
-		$(document).on( 'click', '.press-sync-button', app.pressSyncButton );
-	};
+    app.init = function() {
+        $(document).on( 'click', '.press-sync-button', app.pressSyncButton );
+    };
 
-	app.pressSyncButton = function( click_event ) {
-		app.loadProgressBar();
-		return;
-	}
+    app.pressSyncButton = function() {
+        app.loadProgressBar();
+    }
 
-	app.loadProgressBar = function() {
-		$('.press-sync-button').hide();
-		$('.progress-bar-wrapper,.progress-stats').fadeIn();
+    app.loadProgressBar = function() {
+        $('.press-sync-button').hide();
+        $('.progress-bar-wrapper,.progress-stats').fadeIn();
 
-		$.ajax({
-			method: "POST",
-			url: press_sync.ajax_url,
-			data: {
-				action: 'get_objects_to_sync_count',
-			}
-		}).done(function( response ) {
-			app.updateProgressBar( response.data.objects_to_sync, 0, response.data.total_objects );
+        $.ajax({
+            method: "POST",
+            url: press_sync.ajax_url,
+            data: {
+                action: 'get_objects_to_sync_count',
+            }
+        }).done(function( response ) {
+            app.updateProgressBar( response.data.objects_to_sync, 0, response.data.total_objects );
 
-			if ( response.data.page_size ) {
-				app.PAGE_SIZE = response.data.page_size;
-			}
+            if ( response.data.page_size ) {
+                app.PAGE_SIZE = response.data.page_size;
+            }
 
-			if ( 'all' == response.data.objects_to_sync ) {
-				app.syncAll();
-			} else {
-				app.syncData( 1, response.data.objects_to_sync );
-			}
-		});
-	}
+            if ( 'all' === response.data.objects_to_sync ) {
+                app.syncAll();
+            } else {
+                app.syncData( 1, response.data.objects_to_sync );
+            }
+        });
+    }
 
-	app.syncAll = function() {
-		$.ajax({
-			method: "POST",
-			url: press_sync.ajax_url,
-			data: {
-				action: 'get_order_to_sync_all',
-			}
-		}).done(function( response ) {
-			if ( ! response.success ) {
-				alert( 'There was a connection error. We could not determine the order to sync all objects.' );
-				return;
-			}
+    app.syncAll = function() {
+        $.ajax({
+            method: "POST",
+            url: press_sync.ajax_url,
+            data: {
+                action: 'get_order_to_sync_all',
+            }
+        }).done(function( response ) {
+            if ( ! response.success ) {
+                alert( 'There was a connection error. We could not determine the order to sync all objects.' );
+                return;
+            }
 
-			// Start or batch.
-			app.syncData( 1, null, {
-				order_to_sync_all: response.data,
-			} );
-		});
-	}
+            // Start or batch.
+            app.syncData( 1, null, {
+                order_to_sync_all: response.data,
+            } );
+        });
+    }
 
-	app.updateProgressBar = function( objects_to_sync, total_objects_processed, total_objects, request_time ) {
-		var progress_complete = ( total_objects_processed / total_objects ) * 100;
-		var percent_complete  = Math.floor( progress_complete );
+    app.updateProgressBar = function( objects_to_sync, total_objects_processed, total_objects, request_time ) {
+        const progress_complete = (total_objects_processed / total_objects) * 100;
+        let percent_complete = Math.floor(progress_complete);
 
-		if ( percent_complete > 100 ) {
-			percent_complete = 100;
-		}
+        if ( percent_complete > 100 ) {
+            percent_complete = 100;
+        }
 
-		$('.progress-bar').css('width', progress_complete + '%' ).text( percent_complete + '%' );
+        $('.progress-bar').css('width', progress_complete + '%' ).text( percent_complete + '%' );
 
-		var progress_string = total_objects_processed + '/' + total_objects + ' ' + objects_to_sync + ' synced';
+        let progress_string = total_objects_processed + '/' + total_objects + ' ' + objects_to_sync + ' synced';
 
-		if ( request_time ) {
-			// Estimate time remaining.
-			var remaining_time = ( ( ( total_objects - total_objects_processed ) / app.PAGE_SIZE ) * request_time );
-			remaining_time = remaining_time / 60 / 60;
-			var time_left_suffix = 'hours';
+        if ( request_time ) {
+            // Estimate time remaining.
+            let remaining_time = (((total_objects - total_objects_processed) / app.PAGE_SIZE) * request_time);
+            remaining_time = remaining_time / 60 / 60;
+            let time_left_suffix = 'hours';
 
-			// Shift to minutes.
-			if ( 1 > remaining_time ) {
-				remaining_time = remaining_time * 60;
-				time_left_suffix = 'minutes';
-			}
+            // Shift to minutes.
+            if ( 1 > remaining_time ) {
+                remaining_time = remaining_time * 60;
+                time_left_suffix = 'minutes';
+            }
 
-			// Round to two decimal places, mostly.
-			remaining_time = Math.round( remaining_time * 100 ) / 100;
+            // Round to two decimal places, mostly.
+            remaining_time = Math.round( remaining_time * 100 ) / 100;
 
-			progress_string += ' (' + [ 'Estimated time remaining:', remaining_time, time_left_suffix ].join(' ') + ')';
-		}
+            progress_string += ' (' + [ 'Estimated time remaining:', remaining_time, time_left_suffix ].join(' ') + ')';
+        }
 
-		$('.progress-stats').text( progress_string );
-	}
+        $('.progress-stats').text( progress_string );
+    }
 
-	/**
-	 * Syncs data to the remote site.
-	 *
-	 * @since 0.1.0
-	 * @param int    paged           The page to sync.
-	 * @param string objects_to_sync The object type to sync, may be null if next_args is defined.
-	 * @param object next_args       The arguments to use when syncing a batch of different object types.
-	 */
-	app.syncData = function( paged, objects_to_sync, next_args ) {
-		// "Timing, I'm getting used to it."
-		var start_time = new Date().getTime();
+    /**
+     * Syncs data to the remote site.
+     *
+     * @since 0.1.0
+     * @param {int}    paged            The page to sync.
+     * @param {?string} objects_to_sync The object type to sync, may be null if next_args is defined.
+     * @param {?object} next_args       The arguments to use when syncing a batch of different object types.
+     */
+    app.syncData = function( paged, objects_to_sync, next_args = null ) {
+        // "Timing, I'm getting used to it."
+        const start_time = new Date().getTime();
 
-		// We're syncing all - shift the first object type off the order array.
-		if ( ! objects_to_sync && next_args.order_to_sync_all ) {
-			objects_to_sync = next_args.order_to_sync_all.shift();
-		}
+        // We're syncing all - shift the first object type off the order array.
+        if ( ! objects_to_sync && next_args.order_to_sync_all ) {
+            objects_to_sync = next_args.order_to_sync_all.shift();
+        }
 
-		$.ajax({
-			method: "POST",
-			url: press_sync.ajax_url,
-			data: {
-				action: 'sync_wp_data',
-				paged: paged,
-				objects_to_sync: objects_to_sync
-			}
-		}).done(function( response ) {
-			// Convert request time from milliseconds to seconds.
-			var request_time = ( new Date().getTime() - start_time ) / 1000;
+        $.ajax({
+            method: "POST",
+            url: press_sync.ajax_url,
+            data: {
+                action: 'sync_wp_data',
+                paged: paged,
+                objects_to_sync: objects_to_sync
+            }
+        }).done(function( response ) {
+            // Convert request time from milliseconds to seconds.
+            const request_time = (new Date().getTime() - start_time) / 1000;
 
-			app.updateProgressBar( response.data.ps_objects_to_sync, response.data.total_objects_processed, response.data.total_objects, request_time );
+            app.updateProgressBar( response.data.ps_objects_to_sync, response.data.total_objects_processed, response.data.total_objects, request_time );
 
-			if ( response.data.total_objects_processed >= response.data.total_objects ) {
-				// Start the next batch at page 1.
-				if ( next_args && next_args.order_to_sync_all && next_args.order_to_sync_all.length ) {
-					return app.syncData( 1, null, next_args );
-				}
+            if ( response.data.total_objects_processed >= response.data.total_objects ) {
+                // Start the next batch at page 1.
+                if ( next_args && next_args.order_to_sync_all && next_args.order_to_sync_all.length ) {
+                    return app.syncData( 1, null, next_args );
+                }
 
-				$('.press-sync-button').show();
-				$('.progress-stats').text('Sync completed!');
-				return;
-			}
+                $('.press-sync-button').show();
+                $('.progress-stats').text('Sync completed!');
+                return;
+            }
 
-			app.syncData( response.data.next_page, response.data.ps_objects_to_sync, next_args );
-		});
+            app.syncData( response.data.next_page, response.data.ps_objects_to_sync, next_args );
+        });
 
-	}
+    }
 
-	$( document ).ready( app.init );
+    $( document ).ready( app.init );
 
-	return app;
+    return app;
 
 } )( window, document, jQuery );
--- includes/class-progress.php
+++ includes/class-progress.php
@@ -7,68 +7,73 @@ namespace Press_Sync;
  *
  * @since 0.1.2
  */
-class Progress {
+class Progress
+{
 
-	/**
-	 * Parent plugin class.
-	 *
-	 * @var   Press_Sync
-	 * @since 0.1.0
-	 */
-	protected $plugin = null;
+    /**
+     * Parent plugin class.
+     *
+     * @var   Press_Sync
+     * @since 0.1.0
+     */
+    protected $plugin = null;
 
-	/**
-	 * Constructor.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param Press_Sync $plugin Main plugin object.
-	 */
-	public function __construct( $plugin ) {
-		$this->plugin      = $plugin;
-		$this->cli_enabled = class_exists( '\WP_CLI' );
-	}
+    /**
+     * Constructor.
+     *
+     * @param Press_Sync $plugin Main plugin object.
+     *
+     * @since 0.1.0
+     */
+    public function __construct($plugin)
+    {
+        $this->plugin = $plugin;
+        $this->cli_enabled = class_exists('\WP_CLI');
+    }
 
-	/**
-	 * Begin the progress indication.
-	 *
-	 * @param string  $label The label to supply to the progress indicator.
-	 * @param integer $count The total number of items that will be processed.
-	 */
-	public function start( $label = 'Syncing content', $count = 0 ) {
+    /**
+     * Begin the progress indication.
+     *
+     * @param string  $label The label to supply to the progress indicator.
+     * @param integer $count The total number of items that will be processed.
+     */
+    public function start($label = 'Syncing content', $count = 0)
+    {
 
-		if ( ! $this->cli_enabled ) {
-			return;
-		}
+        if (!$this->cli_enabled) {
+            return;
+        }
 
-		$this->progress = \WP_CLI\Utils\make_progress_bar( $label, $count );
-	}
+        $this->progress = \WP_CLI\Utils\make_progress_bar($label, $count);
+    }
 
-	/**
-	 * Update the progress that some objects have been processed.
-	 */
-	public function tick( $message = '' ) {
+    /**
+     * Update the progress that some objects have been processed.
+     */
+    public function tick($message = '')
+    {
 
-		if ( ! $this->cli_enabled ) {
-			return;
-		}
+        if (!$this->cli_enabled) {
+            return;
+        }
 
-		$this->progress->tick();
+        $this->progress->tick();
 
-		if ( $this->plugin->settings['verbose'] ) {
-			\WP_CLI::line( $message );
-		}
-	}
+        if ($this->plugin->settings['verbose']) {
+            \WP_CLI::line($message);
+        }
+    }
 
-	/**
-	 * Updates the progress that the process is complete.
-	 */
-	public function finish() {
+    /**
+     * Updates the progress that the process is complete.
+     */
+    public function finish()
+    {
 
-		if ( ! $this->cli_enabled ) {
-			return;
-		}
+        if (!$this->cli_enabled) {
+            return;
+        }
 
-		$this->progress->finish();
-	}
+        $this->progress->finish();
+    }
 }
